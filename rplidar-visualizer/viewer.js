542f19f0526d8d5a2123431022bdc22b67d02d09
AdityaGupta1 <adityag@gmail.com>
2018-01-13 17:35:27 -0800

    added panning when zoomed in, still need to add point scaling

--- rplidar-visualizer/viewer.js
+++ rplidar-visualizer/viewer.js
@@ -4,27 +4,34 @@ var cache = [];
 var cacheIndex = 0;
 
 // create WebSocket connection
-var socket = new WebSocket('ws://localhost:8080');
+const socket = new WebSocket('ws://localhost:8080');
 
 // canvas variables
 var canvas;
 var ctx;
 var status;
+var width;
+var height;
 
-// document.onload = function() {
-document.addEventListener("DOMContentLoaded", function(event) {
-    // socket = new WebSocket('ws://localhost:8080');
+// sliders
+var zoomSlider;
 
-    // initialize variables
-    canvas = document.getElementById("field");
-    ctx = canvas.getContext("2d");
-    status = document.getElementById("status");
+document.addEventListener('DOMContentLoaded', function(event) {
+    // initialize canvas variables
+    canvas = document.getElementById('field');
+    ctx = canvas.getContext('2d');
+    status = document.getElementById('status');
+    width = parseInt(canvas.width);
+    height = parseInt(canvas.height);
+
+    // initiate slider variables
+    zoomSlider = document.getElementById('zoom-slider');
 
     // connection opened
     socket.addEventListener('open', function(event) {
-        console.log("connected");
+        console.log('connected');
         status.innerHTML = 'Connected to the WebSocket';
-        socket.send("update");
+        socket.send('update');
     });
 
     // received message from server
@@ -32,19 +39,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
         parseData(JSON.parse(event.data));
         updatePoints();
         console.log('Message from server ', event.data);
-        document.getElementById("slidecontainer").innerHTML = '<h1 class="timestamp">' + cache[cacheIndex].timestamp + '</h1>' +
-            '<input type="range" min="0" max="' + (cache.length - 1) + '" value="' + cacheIndex + '" class="slider">';
+        // document.getElementById('slidecontainer').innerHTML = '<h1 class='timestamp'>' + cache[cacheIndex].timestamp + '</h1>' +
+        //     '<input type='range' min='0' max='' + (cache.length - 1) + '' value='' + cacheIndex + '' class='slider'>';
     });
 
     // connection closed
     socket.addEventListener('close', function(event) {
-        console.log("closed");
+        console.log('closed');
         status.innerHTML = 'Status: Connection Lost';
     });
 
     // connection error
     socket.addEventListener('error', function(event) {
-        console.log("error");
+        console.log('error');
         status.innerHTML = 'Status: Error';
     });
 
@@ -65,4 +72,70 @@ document.addEventListener("DOMContentLoaded", function(event) {
             coordinateQueue[i] = data.scan[i];
         }
     }
+
+    var zoom = 0;
+
+    function getZoom() {
+        return (1 + (0.5 * zoom));
+    }
+
+    var backgroundSize = 'background-size: 100%; ';
+    var backgroundPositionX = 0;
+    var backgroundPositionY = 0;
+
+    function updateCanvasStyle() {
+        canvas.style = backgroundSize + 'background-position: ' + backgroundPositionX + 'px ' + backgroundPositionY + 'px';
+    }
+
+    // do stuff when zoom slider is changed
+    zoomSlider.addEventListener('input', function (evt) {
+        zoom = parseInt(this.value);
+        backgroundSize = 'background-size: ' + (100 * getZoom()) + '%;';
+        backgroundPositionX = 0;
+        backgroundPositionY = 0;
+        updateCanvasStyle();
+    });
+
+    function getMaxMove() {
+        var maxWidth = (width * (getZoom() - 1));
+        var maxHeight = (height * (getZoom() - 1));
+        return {'x': maxWidth, 'y': maxHeight};
+    }
+
+    var clickX;
+    var clickY;
+    var mouseX;
+    var mouseY;
+    var dragInterval;
+
+    canvas.onmousedown = function(event) {
+        clickX = event.x - backgroundPositionX;
+        clickY = event.y - backgroundPositionY;
+
+        dragInterval = setInterval(function() {
+            var dragX = mouseX - clickX;
+            var dragY = mouseY - clickY;
+            var maxX = -getMaxMove().x;
+            var maxY = -getMaxMove().y;
+
+            // make sure dragX and dragY don't go above 0 or go below the limit
+            dragX = dragX < maxX ? maxX : dragX;
+            dragX = dragX > 0 ? 0 : dragX;
+            dragY = dragY < maxY ? maxY : dragY;
+            dragY = dragY > 0 ? 0 : dragY;
+
+            backgroundPositionX = dragX;
+            backgroundPositionY = dragY;
+            updateCanvasStyle();
+        }, 10);
+    };
+
+    canvas.onmousemove = function(event) {
+        mouseX = event.x;
+        mouseY = event.y;
+    }
+
+    document.onmouseup = function(event) {
+        clearInterval(dragInterval);
+    };
 });
