933001bca891d90f8757e995458a790423120c8f
AdityaGupta1 <adityag@gmail.com>
2018-01-19 20:35:07 -0800

    fixed connection to robot

--- rplidar-visualizer/server.js
+++ rplidar-visualizer/server.js
@@ -2,20 +2,20 @@ const WebSocketServer = require('websocket').server;
 const http = require('http');
 const port = 8080;
 
-// const ntClient = require('wpilib-nt-client');
-// const client = new ntClient.Client();
+const ntClient = require('wpilib-nt-client');
+const client = new ntClient.Client();
 
-// client.start((isConnected, err) => {
-//     console.log({ isConnected, err });
-// }, 'roborio-254.local');
-//
-// client.addListener((key, val, type, id) => {
-//     console.log({ key, val, type, id });
-// })
+client.start((connected, error) => {
+    console.log('connected to robot: ' + connected);
+}, 'roborio-254-frc.local');
+
+client.addListener((key, value, type, id) => {
+    console.log({key, value, type, id});
+})
 
 var server = http.createServer(function(request, response) {});
 server.listen(port, function() {});
-console.log('connected on port ' + port);
+console.log('websocket connected on port ' + port);
 
 const socket = new WebSocketServer({
     httpServer: server
