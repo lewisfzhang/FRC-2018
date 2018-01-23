0be07b16c226b34b62a0b3bcd1c974ae49de6a21
SumiGovindaraju <sumi.govindaraju20@bcp.org>
2018-01-22 21:57:14 -0800

    Added test Robot.java class for UDPLidar

--- src/main/java/com/team254/frc2018/Robot.java
+++ src/main/java/com/team254/frc2018/Robot.java
@@ -1,73 +1,44 @@
 package com.team254.frc2018;
 
-import com.team254.lib.util.drivers.RPLidar;
+import com.team254.frc2018.lidar.UDPLidar;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.SerialPort;
+
 
 public class Robot extends IterativeRobot {
-    private RPLidar mRPLidar = new RPLidar(SerialPort.Port.kUSB);
+
+    private UDPLidar mUDPLidar;
+    private Runtime mRuntime;
+    private Process mChezyLidar;
 
     @Override
     public void robotInit() {
-        System.out.println("Robot Init");
+        mRuntime = Runtime.getRuntime();
+
+        try {
+            mChezyLidar = mRuntime.exec("/home/root/chezy_lidar");
+            mChezyLidar.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mUDPLidar = new UDPLidar();
     }
 
     @Override
-    public void disabledInit() { }
+    public void disabledInit() {
+        mChezyLidar.destroy();
+    }
 
     @Override
     public void autonomousInit() { }
 
     @Override
     public void teleopInit() {
-        System.out.println("Teleop Init");
-
-//        mRPLidar.startExpressScan();
-        System.out.println("Initial health: " + mRPLidar.checkHealth());
-        mRPLidar.startMotor();
         try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
+            mChezyLidar = mRuntime.exec("/home/root/chezy_lidar");
+            mChezyLidar.waitFor();
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println("Start Monitor Health: " + mRPLidar.checkHealth());
-//        mRPLidar.forceScan();
-        mRPLidar.startScan();
     }
-
-    @Override
-    public void testInit() { }
-
-
-    @Override
-    public void disabledPeriodic() { }
-
-    @Override
-    public void autonomousPeriodic() { }
-
-    @Override
-    public void teleopPeriodic() {
-        System.out.println("Teleop Periodic");
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-
-//         RPLidar.ExpressScanFrame frame = mRPLidar.readExpressScanPacket();
-//         System.out.println(frame);
-
-
-        byte[] data = mRPLidar.dataDump();
-        System.out.println("Read " + data.length + " units of data");
-
-        for (int i = 0; i < data.length; i++) {
-            System.out.print(Integer.toHexString(Byte.toUnsignedInt(data[i])) + " ");
-        }
-
-        System.out.println("\nHealth: " + mRPLidar.checkHealth());
-    }
-
-    @Override
-    public void testPeriodic() { }
 }
