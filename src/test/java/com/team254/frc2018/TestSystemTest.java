34d7108df00033410c04f0075b44767b4d2f81e4
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2018-01-26 14:34:35 -0800

    Add junit to gradle
    You can now run tests with ./gradlew test

---
+++ src/test/java/com/team254/frc2018/TestSystemTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestSystemTest {
+
+    @Test
+    public void aTest() {
+        // assert statements
+        assertEquals(0, 0, "0 must be 0");
+    }
+}
