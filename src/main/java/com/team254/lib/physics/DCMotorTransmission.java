59b21702f4e09be14ad76822505cabc8edff8422
Jared Russell <jared@team341.com>
2018-02-14 08:02:48 -0800

    More work on physics

--- src/main/java/com/team254/lib/physics/DCMotorTransmission.java
+++ src/main/java/com/team254/lib/physics/DCMotorTransmission.java
@@ -2,87 +2,86 @@ package com.team254.lib.physics;
 
 import com.team254.lib.util.Util;
 
+/**
+ * Model of a DC motor rotating a shaft.  All parameters refer to the output (e.g. should already consider gearing
+ * and efficiency losses).  The motor is assumed to be symmetric forward/reverse.
+ */
 public class DCMotorTransmission {
     // TODO add electrical constants?  (e.g. current)
 
     // All units must be SI!
-    protected final double gear_reduction_;  // ratio
-    protected final double efficiency_;  // unitless, [0, 1]
-    protected final double stall_torque_;  // N m
-    protected final double free_speed_;  // rad/s
-    protected final double nominal_voltage_;  // V
-    protected final double friction_torque_;  // N m (assume identical for both static and rolling friction)
-    protected final double torque_constant_;  // N m
+    protected final double speed_per_volt_;  // rad/s per V (no load)
+    protected final double torque_per_volt_;  // N m per V (stall)
+    protected final double friction_voltage_;  // V
 
-    // Free speed is actual observed speed at nominal voltage (e.g. it should account for speed loss due to friction).
-    public DCMotorTransmission(final double gear_reduction, final double efficiency, final double stall_torque, final double free_speed, final double nominal_voltage, final double friction_torque) {
-        gear_reduction_ = gear_reduction;
-        efficiency_ = efficiency;
-        stall_torque_ = stall_torque;
-        free_speed_ = free_speed;
-        nominal_voltage_ = nominal_voltage;
-        friction_torque_ = friction_torque;
-
-        torque_constant_ = gear_reduction_ * efficiency_ * stall_torque_;
+    public DCMotorTransmission(final double speed_per_volt, final double torque_per_volt, final double
+            friction_voltage) {
+        speed_per_volt_ = speed_per_volt;
+        torque_per_volt_ = torque_per_volt;
+        friction_voltage_ = friction_voltage;
     }
 
-    // TODO add more useful constructors
-
-    public double gear_reduction() {
-        return gear_reduction_;
+    public double speed_per_volt() {
+        return speed_per_volt_;
     }
 
-    public double efficiency() {
-        return efficiency_;
+    public double torque_per_volt() {
+        return torque_per_volt_;
     }
 
-    public double stall_torque() {
-        return stall_torque_;
+    public double friction_voltage() {
+        return friction_voltage_;
     }
 
-    public double free_speed() {
-        return free_speed_;
-    }
-
-    public double nominal_voltage() {
-        return nominal_voltage_;
-    }
-
-    public double friction_torque() {
-        return friction_torque_;
-    }
-
-    public double torque_constant() {
-        return torque_constant_;
-    }
-
-    // TODO interpret free speed/stall torque as actual observed values (compensate for friction).
-    public double getTorqueForVoltage(double output_speed, double voltage) {
-        final double frictionless_torque = torque_constant_ * (-output_speed * gear_reduction_ / free_speed_ + voltage / nominal_voltage_);
-        if (Util.epsilonEquals(Math.abs(output_speed), 0.0)) {
-            // Static friction opposes force/torque.
-            if (frictionless_torque >= 0.0) {
-                return Math.min(0.0, frictionless_torque - friction_torque_);
-            } else {
-                return Math.max(0.0, frictionless_torque + friction_torque_);
-            }
-        }
-        // Dynamic friction opposes motion.
-        if (output_speed > 0.0) {
-            return frictionless_torque - friction_torque_;
+    public double free_speed_at_voltage(final double voltage) {
+        if (voltage > Util.kEpsilon) {
+            return Math.max(0.0, voltage - friction_voltage_) * speed_per_volt_;
+        } else if (voltage < Util.kEpsilon) {
+            return Math.min(0.0, voltage + friction_voltage_) * speed_per_volt_;
         } else {
-            return frictionless_torque + friction_torque_;
-        }
-    }
-
-    public double getVoltageForTorque(double output_speed, double torque) {
-        if (Util.epsilonEquals(Math.abs(torque), 0.0)) {
             return 0.0;
         }
-        if (output_speed > 0.0) {
-            return ((torque + friction_torque_) / torque_constant_ + output_speed * gear_reduction_ / free_speed_) * nominal_voltage_;
+    }
+
+    public double getTorqueForVoltage(final double output_speed, final double voltage) {
+        double effective_voltage = voltage;
+        if (output_speed > Util.kEpsilon) {
+            // Forward motion, rolling friction.
+            effective_voltage -= friction_voltage_;
+        } else if (output_speed < -Util.kEpsilon) {
+            // Reverse motion, rolling friction.
+            effective_voltage += friction_voltage_;
+        } else if (voltage > Util.kEpsilon) {
+            // System is static, forward torque.
+            effective_voltage = Math.max(0.0, voltage - friction_voltage_);
+        } else if (voltage < -Util.kEpsilon) {
+            // System is static, reverse torque.
+            effective_voltage = Math.min(0.0, voltage + friction_voltage_);
         } else {
-            return ((torque - friction_torque_) / torque_constant_ + output_speed * gear_reduction_ / free_speed_) * nominal_voltage_;
+            // System is idle.
+            return 0.0;
         }
+        return torque_per_volt_ * (-output_speed / speed_per_volt_ + effective_voltage);
+    }
+
+    public double getVoltageForTorque(final double output_speed, final double torque) {
+        double friction_voltage = 0.0;
+        if (output_speed > Util.kEpsilon) {
+            // Forward motion, rolling friction.
+            friction_voltage = friction_voltage_;
+        } else if (output_speed < -Util.kEpsilon) {
+            // Reverse motion, rolling friction.
+            friction_voltage = -friction_voltage_;
+        } else if (torque > Util.kEpsilon) {
+            // System is static, forward torque.
+            friction_voltage = friction_voltage_;
+        } else if (torque < -Util.kEpsilon) {
+            // System is static, reverse torque.
+            friction_voltage = -friction_voltage_;
+        } else {
+            // System is idle.
+            return 0.0;
+        }
+        return torque / torque_per_volt_ + output_speed / speed_per_volt_ + friction_voltage;
     }
 }
--- src/main/java/com/team254/lib/physics/DifferentialDrive.java
+++ src/main/java/com/team254/lib/physics/DifferentialDrive.java
@@ -1,40 +1,102 @@
 package com.team254.lib.physics;
 
+import com.team254.lib.util.Util;
+
+import java.util.Arrays;
+
+/**
+ * Dynamic model a differential drive robot.  Note: to simplify things, this math assumes the center of mass is
+ * coincident with the kinematic center of rotation (e.g. midpoint of the center axle).
+ */
 public class DifferentialDrive {
     // All units must be SI!
-    protected final double linear_load_;  // kg, mass as seen by the wheels (includes both chassis mass and inertia in drivetrain)
-    protected final double angular_load_;  // kg, equivalent mass as seen by the wheels (includes chassis rotational inertia and inertia in drivetrain)
+
+    // TODO
+    // deal with the case that one side is moving but the other side is stationary and has not broken static
+    // friction (so the whole robot pivots about the stationary side).  Equation for this looks like:
+    // F_l + F_r = linear_load*a
+    // F_r - F_l = angular_load*alpha
+    // s = Sgn(a/alpha)
+    // s * effective_wheelbase_width = a/alpha
+    // ~
+    // F_r * (s * linear_load * effective_wheelbase_width - angular_load) == F_l * (s * linear_load *
+    // effective_wheelbase_width + angular_load)
+    // Need to then check if the force at the stationary side breaks static friction.
+
+    // Equivalent mass when accelerating purely linearly, in kg.
+    // This is "equivalent" in that it also absorbs the effects of drivetrain inertia.
+    // Theoretical value:
+    // = m_robot + I_transmissions / r_wheel^2
+    protected final double linear_inertia_;
+
+    // Equivalent load when accelerating purely angularly, in kg*m.
+    // Normally a moment of inertia is in kg*m^2, but in this case we always know the radius at which torques are
+    // applied so can cancel that term.
+    // Theoretical value:
+    // = I_robot_about_com / wheelbase_radius + I_transmissions / r_wheel
+    protected final double angular_inertia_;
+
     protected final double wheel_radius_;  // m
+
+    // "Effective" kinematic wheelbase radius.  Might be larger than theoretical to compensate for skid steer.
     protected final double effective_wheelbase_radius_;  // m
     protected final DCMotorTransmission left_transmission_;
     protected final DCMotorTransmission right_transmission_;
-    public DifferentialDrive(final double linear_load,
-                             final double angular_load,
+
+    public DifferentialDrive(final double linear_inertia,
+                             final double angular_inertia,
                              final double wheel_radius,
                              final double effective_wheelbase_radius,
                              final DCMotorTransmission left_transmission,
                              final DCMotorTransmission right_transmission) {
-        linear_load_ = linear_load;
-        angular_load_ = angular_load;
+        linear_inertia_ = linear_inertia;
+        angular_inertia_ = angular_inertia;
         wheel_radius_ = wheel_radius;
         effective_wheelbase_radius_ = effective_wheelbase_radius;
         left_transmission_ = left_transmission;
         right_transmission_ = right_transmission;
     }
 
+    public double linear_inertia() {
+        return linear_inertia_;
+    }
+
+    public double angular_inertia() {
+        return angular_inertia_;
+    }
+
+    public double wheel_radius() {
+        return wheel_radius_;
+    }
+
+    public double effective_wheelbase_radius() {
+        return effective_wheelbase_radius_;
+    }
+
+    public DCMotorTransmission left_transmission() {
+        return left_transmission_;
+    }
+
+    public DCMotorTransmission right_transmission() {
+        return right_transmission_;
+    }
+
     // Input/output could be either velocity or acceleration...the math is the same.
     public ChassisState solveForwardKinematics(final WheelState wheel_motion) {
         ChassisState chassis_motion = new ChassisState();
         chassis_motion.linear = wheel_radius_ * (wheel_motion.right + wheel_motion.left) / 2.0;
-        chassis_motion.angular = wheel_radius_ * (wheel_motion.right - wheel_motion.left) / effective_wheelbase_radius_;
+        chassis_motion.angular = wheel_radius_ * (wheel_motion.right - wheel_motion.left) / (2.0 *
+                effective_wheelbase_radius_);
         return chassis_motion;
     }
 
     // Input/output could be either velocity or acceleration...the math is the same.
     public WheelState solveInverseKinematics(final ChassisState chassis_motion) {
         WheelState wheel_motion = new WheelState();
-        wheel_motion.left = (2.0 * chassis_motion.linear - effective_wheelbase_radius_ * chassis_motion.angular) / (2.0 * wheel_radius_);
-        wheel_motion.right = (2.0 * chassis_motion.linear + effective_wheelbase_radius_ * chassis_motion.angular) / (2.0 * wheel_radius_);
+        wheel_motion.left = (chassis_motion.linear - effective_wheelbase_radius_ * chassis_motion.angular) /
+                wheel_radius_;
+        wheel_motion.right = (chassis_motion.linear + effective_wheelbase_radius_ * chassis_motion.angular) /
+                wheel_radius_;
         return wheel_motion;
     }
 
@@ -57,20 +119,42 @@ public class DifferentialDrive {
         return dynamics;
     }
 
+    // Assumptions about dynamics: velocities and voltages provided.
     public void solveForwardDynamics(DriveDynamics dynamics) {
-        // TODO deal with the case that one side is moving but the other side is stationary and has not broken static friction (so the whole robot pivots about the stationary side).
-        dynamics.wheel_torque.left = left_transmission_.getTorqueForVoltage(dynamics.wheel_velocity.left, dynamics.voltage.left);
-        dynamics.wheel_torque.right = right_transmission_.getTorqueForVoltage(dynamics.wheel_velocity.right, dynamics.voltage.right);
+        final boolean left_stationary = Util.epsilonEquals(dynamics.wheel_velocity.left, 0.0) && Math.abs(dynamics
+                .voltage.left) < left_transmission_.friction_voltage();
+        final boolean right_stationary = Util.epsilonEquals(dynamics.wheel_velocity.right, 0.0) && Math.abs(dynamics
+                .voltage.right) < right_transmission_.friction_voltage();
+        if (left_stationary && right_stationary) {
+            // Neither side breaks static friction.
+            dynamics.wheel_torque.left = dynamics.wheel_torque.right = 0.0;
+            dynamics.chassis_acceleration.linear = dynamics.chassis_acceleration.angular = 0.0;
+            dynamics.wheel_acceleration.left = dynamics.wheel_acceleration.right = 0.0;
+            return;
+        }
 
-        dynamics.chassis_acceleration.linear = (dynamics.wheel_torque.right + dynamics.wheel_torque.left) / (wheel_radius_ * linear_load_);
-        dynamics.chassis_acceleration.angular = (dynamics.wheel_torque.right - dynamics.wheel_torque.left) / (wheel_radius_ * angular_load_ * effective_wheelbase_radius_);
+        // Solve for motor torques generated on each side.
+        dynamics.wheel_torque.left = left_transmission_.getTorqueForVoltage(dynamics.wheel_velocity.left, dynamics
+                .voltage.left);
+        dynamics.wheel_torque.right = right_transmission_.getTorqueForVoltage(dynamics.wheel_velocity.right, dynamics
+                .voltage.right);
 
-        dynamics.wheel_acceleration.left = dynamics.chassis_acceleration.linear - dynamics.chassis_acceleration.angular * effective_wheelbase_radius_;
-        dynamics.wheel_acceleration.right = dynamics.chassis_acceleration.linear + dynamics.chassis_acceleration.angular * effective_wheelbase_radius_;
+        // Add forces and torques about the center of mass.
+        dynamics.chassis_acceleration.linear = (dynamics.wheel_torque.right + dynamics.wheel_torque.left) /
+                (wheel_radius_ * linear_inertia_);
+        dynamics.chassis_acceleration.angular = (dynamics.wheel_torque.right - dynamics.wheel_torque.left) /
+                (wheel_radius_ * angular_inertia_);
+
+        // Resolve chassis accelerations to each wheel.
+        dynamics.wheel_acceleration.left = dynamics.chassis_acceleration.linear - dynamics.chassis_acceleration
+                .angular * effective_wheelbase_radius_;
+        dynamics.wheel_acceleration.right = dynamics.chassis_acceleration.linear + dynamics.chassis_acceleration
+                .angular * effective_wheelbase_radius_;
     }
 
     // Solve for torques and voltages.
-    public DriveDynamics solveInverseDynamics(final ChassisState chassis_velocity, final ChassisState chassis_acceleration) {
+    public DriveDynamics solveInverseDynamics(final ChassisState chassis_velocity, final ChassisState
+            chassis_acceleration) {
         DriveDynamics dynamics = new DriveDynamics();
         dynamics.chassis_velocity = chassis_velocity;
         dynamics.chassis_acceleration = chassis_acceleration;
@@ -90,22 +174,68 @@ public class DifferentialDrive {
         return dynamics;
     }
 
+    // Assumptions about dynamics: velocities and accelerations provided.
     public void solveInverseDynamics(DriveDynamics dynamics) {
-        dynamics.wheel_torque.left = wheel_radius_ / 2.0 * (dynamics.chassis_acceleration.linear * linear_load_ - dynamics.chassis_acceleration.angular * angular_load_ * effective_wheelbase_radius_);
-        dynamics.wheel_torque.right = wheel_radius_ / 2.0 * (dynamics.chassis_acceleration.linear * linear_load_ + dynamics.chassis_acceleration.angular * angular_load_ * effective_wheelbase_radius_);
+        // Determine the necessary torques on the left and right wheels to produce the desired wheel accelerations.
+        dynamics.wheel_torque.left = wheel_radius_ / 2.0 * (dynamics.chassis_acceleration.linear * linear_inertia_ -
+                dynamics.chassis_acceleration.angular * angular_inertia_);
+        dynamics.wheel_torque.right = wheel_radius_ / 2.0 * (dynamics.chassis_acceleration.linear * linear_inertia_ +
+                dynamics.chassis_acceleration.angular * angular_inertia_);
 
-        dynamics.voltage.left = left_transmission_.getVoltageForTorque(dynamics.wheel_velocity.left, dynamics.wheel_torque.left);
-        dynamics.voltage.right = right_transmission_.getVoltageForTorque(dynamics.wheel_velocity.right, dynamics.wheel_torque.right);
+        // Solve for input voltages.
+        dynamics.voltage.left = left_transmission_.getVoltageForTorque(dynamics.wheel_velocity.left, dynamics
+                .wheel_torque.left);
+        dynamics.voltage.right = right_transmission_.getVoltageForTorque(dynamics.wheel_velocity.right, dynamics
+                .wheel_torque.right);
     }
 
     public double getMaxAbsVelocity(double curvature, double max_abs_voltage) {
-        // TODO
-        return 0.0;
+        // k = w / v
+        // v = r_w*(wr + wl) / 2
+        // w = r_w*(wr - wl) / (2 * r_wb)
+        // Plug in max_abs_voltage for each wheel.
+        if (Double.isInfinite(curvature)) {
+            // Turn in place.
+            return Double.POSITIVE_INFINITY;
+        }
+        final double left_speed_at_max_voltage = left_transmission_.free_speed_at_voltage(max_abs_voltage);
+        final double right_speed_at_max_voltage = right_transmission_.free_speed_at_voltage(max_abs_voltage);
+        if (Util.epsilonEquals(curvature, 0.0)) {
+            return wheel_radius_ * Math.min(left_speed_at_max_voltage, right_speed_at_max_voltage);
+        }
+
+        final double right_speed_if_left_max = left_speed_at_max_voltage * (effective_wheelbase_radius_ * curvature +
+                1.0) / (1.0 - effective_wheelbase_radius_ * curvature);
+        if (Math.abs(right_speed_if_left_max) <= right_speed_at_max_voltage) {
+            return wheel_radius_ * (left_speed_at_max_voltage + right_speed_if_left_max) / 2.0;
+        }
+        final double left_speed_if_right_max = right_speed_at_max_voltage * (1.0 - effective_wheelbase_radius_ *
+                curvature) / (1.0 + effective_wheelbase_radius_ * curvature);
+        assert Math.abs(left_speed_if_right_max) <= left_speed_at_max_voltage;
+        return wheel_radius_ * (right_speed_at_max_voltage + left_speed_if_right_max) / 2.0;
     }
 
-    public double getMinMaxAcceleration(double curvature, double com_velocity, double max_abs_voltage) {
-        // TODO
-        return 0.0;
+    public static class MinMax {
+        public double min;
+        public double max;
+    }
+
+    public MinMax getMinMaxAcceleration(final ChassisState chassis_velocity, double max_abs_voltage) {
+        MinMax result = new MinMax();
+        result.min = Double.POSITIVE_INFINITY;
+        result.max = Double.NEGATIVE_INFINITY;
+        // Check all four cases and record the min and max valid accelerations.
+        for (boolean left : Arrays.asList(false, true)) {
+            for (double sign : Arrays.asList(1.0, -1.0)) {
+                final DCMotorTransmission transmission = left ? left_transmission_ : right_transmission_;
+                // Set one side of the drive to max_abs_voltage and solve for what the other side would have to be.
+                //dynamics.wheel_torque.set(left, transmission.getTorqueForVoltage(dynamics.wheel_velocity.get(left),
+                // sign * max_abs_voltage));
+                // TODO
+            }
+        }
+        // Look at the resulting voltages.
+        return result;
     }
 
     // Can refer to velocity or acceleration depending on context.
@@ -117,6 +247,7 @@ public class DifferentialDrive {
             this.linear = linear;
             this.angular = angular;
         }
+
         public ChassisState() {
         }
     }
@@ -130,7 +261,20 @@ public class DifferentialDrive {
             this.left = left;
             this.right = right;
         }
+
         public WheelState() {
+        }
+
+        public double get(boolean get_left) {
+            return get_left ? left : right;
+        }
+
+        public void set(boolean set_left, double val) {
+            if (set_left) {
+                left = val;
+            } else {
+                right = val;
+            }
         }
     }
 
