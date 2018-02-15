fe53c3d4e5ce1c683ae28dd5b5ca15a853457fc6
SuperMario210 <Sup3rMar1o210@gmail.com>
2018-02-14 22:31:49 -0800

    added quintic hermite splines

---
+++ src/test/java/com/team254/lib/spline/QuinticHermiteOptimizerTest.java
@@ -0,0 +1,43 @@
+package com.team254.lib.spline;
+
+import com.team254.lib.geometry.Pose2d;
+import com.team254.lib.geometry.Rotation2d;
+import com.team254.lib.geometry.Translation2d;
+import com.team254.lib.util.Util;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class QuinticHermiteOptimizerTest {
+    public static final double kTestEpsilon = Util.kEpsilon;
+
+    @Test
+    public void test() {
+        Pose2d a = new Pose2d(new Translation2d(0, 100), Rotation2d.fromDegrees(270));
+        Pose2d b = new Pose2d(new Translation2d(50, 0), Rotation2d.fromDegrees(0));
+        Pose2d c = new Pose2d(new Translation2d(100, 100), Rotation2d.fromDegrees(90));
+
+        List<QuinticHermiteSpline> splines = new ArrayList<>();
+        splines.add(new QuinticHermiteSpline(a, b));
+        splines.add(new QuinticHermiteSpline(b, c));
+
+        assertEquals(QuinticHermiteSpline.optimizeSpline(splines), 0.013045726574273941, kTestEpsilon);
+
+        Pose2d d = new Pose2d(new Translation2d(0, 0), Rotation2d.fromDegrees(90));
+        Pose2d e = new Pose2d(new Translation2d(0, 50), Rotation2d.fromDegrees(0));
+        Pose2d f = new Pose2d(new Translation2d(100, 0), Rotation2d.fromDegrees(90));
+        Pose2d g = new Pose2d(new Translation2d(100, 100), Rotation2d.fromDegrees(0));
+
+        List<QuinticHermiteSpline> splines1 = new ArrayList<>();
+        splines1.add(new QuinticHermiteSpline(d, e));
+        splines1.add(new QuinticHermiteSpline(e, f));
+        splines1.add(new QuinticHermiteSpline(f, g));
+
+        assertEquals(QuinticHermiteSpline.optimizeSpline(splines1), 0.17097652323999749, kTestEpsilon);
+
+    }
+}
