f45f75478e7d5997ec4bc701306440af7ef9e8e5
Jared Russell <jared@team341.com>
2018-02-15 19:52:37 -0800

    Create timing constraints from differential drive

--- src/main/java/com/team254/lib/trajectory/timing/DifferentialDriveDynamicsConstraint.java
+++ src/main/java/com/team254/lib/trajectory/timing/DifferentialDriveDynamicsConstraint.java
@@ -2,27 +2,28 @@ package com.team254.lib.trajectory.timing;
 
 import com.team254.lib.geometry.ICurvature;
 import com.team254.lib.geometry.IPose2d;
+import com.team254.lib.physics.DifferentialDrive;
 
 public class DifferentialDriveDynamicsConstraint<S extends IPose2d<S> & ICurvature<S>> implements TimingConstraint<S> {
 
-    // TODO: Implement constraints based on a differential drive (ensuring that we don't exceed voltage/current
-    // limits when following a trajectory).
+    protected final DifferentialDrive drive_;
+    protected final double abs_voltage_limit_;
 
-    public DifferentialDriveDynamicsConstraint() {
-
+    public DifferentialDriveDynamicsConstraint(final DifferentialDrive drive, double abs_voltage_limit) {
+        drive_ = drive;
+        abs_voltage_limit_ = abs_voltage_limit;
     }
 
     @Override
     public double getMaxVelocity(S state) {
-        // TODO Auto-generated method stub
-        return 0;
+        return drive_.getMaxAbsVelocity(state.getCurvature(), abs_voltage_limit_);
     }
 
     @Override
-    public com.team254.lib.trajectory.timing.TimingConstraint.MinMaxAcceleration getMinMaxAcceleration(S state,
-                                                                                                       double velocity) {
-        // TODO Auto-generated method stub
-        return null;
+    public MinMaxAcceleration getMinMaxAcceleration(S state,
+                                                    double velocity) {
+        DifferentialDrive.MinMax min_max = drive_.getMinMaxAcceleration(new DifferentialDrive.ChassisState(velocity,
+                state.getCurvature() * velocity), state.getCurvature(), abs_voltage_limit_);
+        return new MinMaxAcceleration(min_max.min, min_max.max);
     }
-
 }
