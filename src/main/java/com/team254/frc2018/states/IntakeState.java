765e2b4ab14d3a605395a8cf21e6b2155b99bcb1
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2018-02-16 18:03:48 -0800

    Refactor intake state machine, add tests

---
+++ src/main/java/com/team254/frc2018/states/IntakeState.java
@@ -0,0 +1,26 @@
+package com.team254.frc2018.states;
+
+public class IntakeState {
+    public enum JawState {
+        OPEN,
+        CLOSED,
+        CLAMPED
+    }
+    public JawState jawState = JawState.CLOSED;
+    public double leftMotor = 0;
+    public double rightMotor = 0;
+    public double wristAngle = 0; // needed to enforce clamping limits
+
+    public boolean leftCubeSensorTriggered = false;
+    public boolean rightCubeSensorTriggered = false;
+
+    public void setPower(double power) {
+        leftMotor = rightMotor = power;
+    }
+
+    public boolean seesCube() {
+        return leftCubeSensorTriggered || rightCubeSensorTriggered;
+    }
+
+
+}
