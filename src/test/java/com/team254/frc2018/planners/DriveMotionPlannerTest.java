63f958153d3ce946908d1e8c9bf92c5eaacea32a
Jared Russell <jared@team341.com>
2018-03-03 19:45:42 -0800

    Add test case

--- src/test/java/com/team254/frc2018/planners/DriveMotionPlannerTest.java
+++ src/test/java/com/team254/frc2018/planners/DriveMotionPlannerTest.java
@@ -1,8 +1,11 @@
 package com.team254.frc2018.planners;
 
+import com.team254.frc2018.Constants;
+import com.team254.frc2018.Kinematics;
 import com.team254.lib.geometry.Pose2d;
 import com.team254.lib.geometry.Rotation2d;
 import com.team254.lib.geometry.Translation2d;
+import com.team254.lib.geometry.Twist2d;
 import com.team254.lib.trajectory.TimedView;
 import com.team254.lib.trajectory.TrajectoryIterator;
 import org.junit.jupiter.api.Test;
@@ -26,7 +29,7 @@ public class DriveMotionPlannerTest {
             Pose2d pose = motion_planner.setpoint().state().getPose();
             while (!motion_planner.isDone()) {
                 motion_planner.update(t, pose);
-                pose = motion_planner.mSetpoint.state().getPose().transformBy(new Pose2d(new Translation2d(0.0, 1.0), Rotation2d.fromDegrees(2.0)));
+                pose = motion_planner.mSetpoint.state().getPose();//.transformBy(new Pose2d(new Translation2d(0.0, 1.0), Rotation2d.fromDegrees(2.0)));
                 System.out.println(t + "," + motion_planner.toCSV());
                 t += 0.01;
             }
@@ -43,7 +46,7 @@ public class DriveMotionPlannerTest {
             Pose2d pose = motion_planner.setpoint().state().getPose();
             while (!motion_planner.isDone()) {
                 motion_planner.update(t, pose);
-                pose = motion_planner.mSetpoint.state().getPose().transformBy(new Pose2d(new Translation2d(0.0, -1.0), Rotation2d.fromDegrees(-2.0)));
+                pose = motion_planner.mSetpoint.state().getPose();//.transformBy(new Pose2d(new Translation2d(0.0, -1.0), Rotation2d.fromDegrees(-2.0)));
                 System.out.println(t + "," + motion_planner.toCSV());
                 t += 0.01;
             }
@@ -66,5 +69,26 @@ public class DriveMotionPlannerTest {
             }
         }
 
+    }
+
+    @Test
+    public void test2() {
+        final DriveMotionPlanner motion_planner = new DriveMotionPlanner();
+        motion_planner.setTrajectory(new TrajectoryIterator<>(new TimedView<>(motion_planner.generateTrajectory
+                (Arrays.asList(new Pose2d(new Translation2d(0.0, 0.0), Rotation2d.identity()),
+                        new Pose2d(new Translation2d(120.0, -36.0), Rotation2d.identity()),
+                        new Pose2d(new Translation2d(240.0, -36.0), Rotation2d.identity())),
+                        null,
+                        120.0, 120.0, 10.0))));
+        final double dt = 0.01;
+        double t = 0.0;
+        Pose2d pose = motion_planner.setpoint().state().getPose();
+        while (!motion_planner.isDone()) {
+            DriveMotionPlanner.Output output = motion_planner.update(t, pose);
+            Twist2d delta = Kinematics.forwardKinematics(output.left_velocity * dt * Constants.kDriveWheelDiameterInches / 2.0, output.right_velocity * dt * Constants.kDriveWheelDiameterInches / 2.0);
+            pose = pose.transformBy(Pose2d.exp(delta));
+            t += dt;
+        }
+        System.out.println(pose);
     }
 }
