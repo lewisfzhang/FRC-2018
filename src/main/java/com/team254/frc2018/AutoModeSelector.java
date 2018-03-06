7863f995363c67bb12ba0907b89cf486fd0ac244
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2018-03-05 17:00:43 -0800

    Wire in starting position chooser

--- src/main/java/com/team254/frc2018/AutoModeSelector.java
+++ src/main/java/com/team254/frc2018/AutoModeSelector.java
@@ -1,5 +1,6 @@
 package com.team254.frc2018;
 
+import com.sun.tools.javadoc.Start;
 import com.team254.frc2018.auto.AutoModeBase;
 import com.team254.frc2018.auto.creators.AutoModeCreator;
 import com.team254.frc2018.auto.creators.SimpleSwitchModeCreator;
@@ -11,6 +12,12 @@ import java.util.Optional;
 
 public class AutoModeSelector {
 
+    enum StartingPosition {
+        LEFT,
+        CENTER,
+        RIGHT,
+    };
+
     enum DesiredMode {
         DO_NOTHING,
         SIMPLE_SWITCH,
@@ -19,10 +26,12 @@ public class AutoModeSelector {
         ADVANCED, // This uses 4 additional sendable choosers to pick one for each field state combo
     };
     private DesiredMode mCachedDesiredMode = null;
+    private StartingPosition mCachedPosition = null;
 
     private Optional<AutoModeCreator> mCreator = Optional.empty();
 
     private SendableChooser<DesiredMode> mModeChooser;
+    private SendableChooser<StartingPosition> mPositionChooser;
 
     public AutoModeSelector() {
         mModeChooser = new SendableChooser<>();
@@ -31,24 +40,33 @@ public class AutoModeSelector {
         mModeChooser.addObject("Scale AND Switch", DesiredMode.SCALE_AND_SWITCH);
         mModeChooser.addObject("Only Scale", DesiredMode.ONLY_SCALE);
         SmartDashboard.putData("Auto mode", mModeChooser);
+
+        mPositionChooser = new SendableChooser<>();
+        mPositionChooser.addDefault("Right", StartingPosition.RIGHT);
+        mPositionChooser.addObject("Center", StartingPosition.CENTER);
+        mPositionChooser.addObject("Left", StartingPosition.LEFT);
+        SmartDashboard.putData("Starting Position", mPositionChooser);
+
     }
 
     public void updateModeCreator() {
         DesiredMode desiredMode = mModeChooser.getSelected();
-        if(mCachedDesiredMode != desiredMode) {
+        StartingPosition position = mPositionChooser.getSelected();
+        if(mCachedDesiredMode != desiredMode || position != mCachedPosition) {
             System.out.println("Auto selection changed, updating creator" + desiredMode);
-            mCreator = getCreatorForParams(desiredMode);
+            mCreator = getCreatorForParams(desiredMode, position);
         }
         mCachedDesiredMode = desiredMode;
+        mCachedPosition = position;
     }
 
-    private Optional<AutoModeCreator> getCreatorForParams(DesiredMode mode) {
+    private Optional<AutoModeCreator> getCreatorForParams(DesiredMode mode, StartingPosition position) {
+        boolean startOnLeft = StartingPosition.LEFT == position;
         switch (mode) {
             case SIMPLE_SWITCH:
                 return Optional.of(new SimpleSwitchModeCreator());
             case SCALE_AND_SWITCH:
-                return Optional.of(new SwitchAndScaleAutoModeCreator(false));
-                // TODO: Make this work for th
+                return Optional.of(new SwitchAndScaleAutoModeCreator(startOnLeft));
             default:
                 break;
         }
