ad83aad700e09b1089643453fe10fabfdb557504
SuperMario210 <Sup3rMar1o210@gmail.com>
2018-03-05 23:20:55 -0800

    fix quickturn issue

--- src/main/java/com/team254/lib/util/CheesyDriveHelper.java
+++ src/main/java/com/team254/lib/util/CheesyDriveHelper.java
@@ -27,16 +27,16 @@ public class CheesyDriveHelper {
 
     private static final double kQuickStopDeadband = 0.5;
     private static final double kQuickStopWeight = 0.1;
-    private static final double kQuickStopScalar = 5.0; // This number is tuned for corner omni wheels
+    private static final double kQuickStopScalar = 5.0;
 
     private double mOldWheel = 0.0;
     private double mQuickStopAccumlator = 0.0;
     private double mNegInertiaAccumlator = 0.0;
 
-    public DriveSignal cheesyDrive(double throttle, double wheelIn, boolean isQuickTurn,
+    public DriveSignal cheesyDrive(double throttle, double wheel, boolean isQuickTurn,
                                    boolean isHighGear) {
 
-        double wheel = handleDeadband(wheelIn, kWheelDeadband);
+        wheel = handleDeadband(wheel, kWheelDeadband);
         throttle = handleDeadband(throttle, kThrottleDeadband);
 
         double negInertia = wheel - mOldWheel;
@@ -97,7 +97,7 @@ public class CheesyDriveHelper {
         linearPower = throttle;
 
         // Quickturn!
-        if (isQuickTurn && Math.abs(wheelIn) > kQuickStopDeadband) {
+        if (isQuickTurn) {
             if (Math.abs(linearPower) < kQuickStopDeadband) {
                 double alpha = kQuickStopWeight;
                 mQuickStopAccumlator = (1 - alpha) * mQuickStopAccumlator
