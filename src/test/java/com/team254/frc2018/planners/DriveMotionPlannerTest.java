db5e89f087a65d00a0ba6a8e0dc1d21cf6e9e8ad
Jared Russell <jared@team341.com>
2018-03-06 23:46:30 -0800

    Better test cases

--- src/test/java/com/team254/frc2018/planners/DriveMotionPlannerTest.java
+++ src/test/java/com/team254/frc2018/planners/DriveMotionPlannerTest.java
@@ -81,31 +81,43 @@ public class DriveMotionPlannerTest {
     @Test
     public void testForwardReverseSame() {
         DriveMotionPlanner fwd_motion_planner = new DriveMotionPlanner();
-        fwd_motion_planner.setFollowerType(DriveMotionPlanner.FollowerType.FEEDFORWARD_ONLY);
+        fwd_motion_planner.setFollowerType(DriveMotionPlanner.FollowerType.PURE_PURSUIT);
         fwd_motion_planner.setTrajectory(new TrajectoryIterator<>(new TimedView<>(fwd_motion_planner.generateTrajectory
-                (false, Arrays.asList(new Pose2d(new Translation2d(0.0, 0.0), Rotation2d.identity()),
+                (false, Arrays.asList(new Pose2d(new Translation2d(10.0, 10.0), Rotation2d.fromDegrees(5.0)),
                         new Pose2d(new Translation2d(120.0, 36.0), Rotation2d.identity()),
-                        new Pose2d(new Translation2d(240.0, 0.0), Rotation2d.identity())),
+                        new Pose2d(new Translation2d(230.0, 10.0), Rotation2d.fromDegrees(-5.0))),
                         null,
-                        120.0, 120.0, 10.0))));
+                        120.0, 120.0, 5.0))));
         DriveMotionPlanner rev_motion_planner = new DriveMotionPlanner();
+        rev_motion_planner.setFollowerType(DriveMotionPlanner.FollowerType.PURE_PURSUIT);
         rev_motion_planner.setTrajectory(new TrajectoryIterator<>(new TimedView<>(rev_motion_planner.generateTrajectory
-                (true, Arrays.asList(new Pose2d(new Translation2d(240.0, 0.0), Rotation2d.identity()),
+                (true, Arrays.asList(new Pose2d(new Translation2d(230.0, 10.0), Rotation2d.fromDegrees(-5.0)),
                         new Pose2d(new Translation2d(120.0, 36.0), Rotation2d.identity()),
-                        new Pose2d(new Translation2d(0.0, 0.0), Rotation2d.identity())),
+                        new Pose2d(new Translation2d(10.0, 10.0), Rotation2d.fromDegrees(5.0))),
                         null,
-                        120.0, 120.0, 10.0))));
+                        120.0, 120.0, 5.0))));
 
+        final double dt = 0.01;
         double t = 0.0;
-        Pose2d fwd_pose = fwd_motion_planner.setpoint().state().getPose();
-        Pose2d rev_pose = rev_motion_planner.setpoint().state().getPose();
+        Pose2d start_error_fwd = new Pose2d(2.0, 3.0, Rotation2d.fromDegrees(2.0));
+        Pose2d start_error_rev = new Pose2d(-2.0, 3.0, Rotation2d.fromDegrees(-2.0));
+        Pose2d fwd_pose = fwd_motion_planner.setpoint().state().getPose().transformBy(start_error_fwd);
+        Pose2d rev_pose = rev_motion_planner.setpoint().state().getPose().transformBy(start_error_rev);
         while (!fwd_motion_planner.isDone() || !rev_motion_planner.isDone()) {
-            fwd_motion_planner.update(t, fwd_pose);
-            fwd_pose = fwd_motion_planner.mSetpoint.state().getPose();
-            rev_motion_planner.update(t, rev_pose);
-            rev_pose = rev_motion_planner.mSetpoint.state().getPose();
+            DriveMotionPlanner.Output fwd_output = fwd_motion_planner.update(t, fwd_pose);
+            Twist2d fwd_delta = Kinematics.forwardKinematics(fwd_output.left_velocity * dt * Constants
+                    .kDriveWheelDiameterInches / 2.0, fwd_output.right_velocity * dt * Constants
+                    .kDriveWheelDiameterInches / 2.0);
+            fwd_pose = fwd_pose.transformBy(Pose2d.exp(fwd_delta));
+            //System.out.println("FWD Delta: " + fwd_delta + ", Pose: " + fwd_pose);
+            DriveMotionPlanner.Output rev_output = rev_motion_planner.update(t, rev_pose);
+            Twist2d rev_delta = Kinematics.forwardKinematics(rev_output.left_velocity * dt * Constants
+                    .kDriveWheelDiameterInches / 2.0, rev_output.right_velocity * dt * Constants
+                    .kDriveWheelDiameterInches / 2.0);
+            rev_pose = rev_pose.transformBy(Pose2d.exp(rev_delta));
+            //System.out.println("REV Delta: " + rev_delta + ", Pose: " + rev_pose);
             System.out.println(fwd_motion_planner.toCSV() + "," + rev_motion_planner.toCSV());
-            t += 0.01;
+            t += dt;
         }
     }
 
