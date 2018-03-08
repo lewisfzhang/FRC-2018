e08839b64f7e142492e0facc3e4cc8b7c828be23
Solomon Chang <changsolomon8@gmail.com>
2018-03-08 14:12:56 -0800

    Remove soft limits in climb mode

--- src/main/java/com/team254/frc2018/Robot.java
+++ src/main/java/com/team254/frc2018/Robot.java
@@ -253,6 +253,7 @@ public class Robot extends IterativeRobot {
             }
 
             if (mInHangMode) {
+                mElevator.enableSoftLimits(false);
                 mLED.setWantedAction(LED.WantedAction.DISPLAY_HANG);
                 if (mControlBoard.getDeployForks()) {
                     // This is fire once only!
@@ -269,6 +270,7 @@ public class Robot extends IterativeRobot {
                 mSuperstructure.setUnlockHookSolenoid(true);
             } else {
                 mSuperstructure.setUnlockHookSolenoid(false);
+                mElevator.enableSoftLimits(true);
                 mLED.setWantedAction(LED.WantedAction.DISPLAY_INTAKE);
 
                 // Intake/Shoot
