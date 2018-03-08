2c6408f4e077399aa6f4897b7306013932fd6d11
SuperMario210 <Sup3rMar1o210@gmail.com>
2018-03-08 16:48:21 -0700

    adjust shoot power

--- src/main/java/com/team254/frc2018/statemachines/IntakeStateMachine.java
+++ src/main/java/com/team254/frc2018/statemachines/IntakeStateMachine.java
@@ -7,8 +7,8 @@ import com.team254.lib.util.TimeDelayedBoolean;
 
 public class IntakeStateMachine {
     public final static double kActuationTime = 0.0;
-    public final static double kStrongShootSetpoint = 1.0;
-    public final static double kWeakShootSetpoint = .65;
+    public final static double kStrongShootSetpoint = 0.65;
+    public final static double kWeakShootSetpoint = .5;
     public final static double kIntakeCubeSetpoint = -1.0;
     public final static double kHoldSetpoint = 0.0;
     public final static double kLostCubeTime = 0.25;
