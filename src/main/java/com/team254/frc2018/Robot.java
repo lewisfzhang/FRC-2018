e761180c9711c0e2380828d61ca8ac03981747fd
SuperMario210 <Sup3rMar1o210@gmail.com>
2018-03-09 12:54:48 -0700

    added deadband to hang mode jog and made forklifts retract out of hang mode

--- src/main/java/com/team254/frc2018/Robot.java
+++ src/main/java/com/team254/frc2018/Robot.java
@@ -14,6 +14,7 @@ import com.team254.lib.geometry.Pose2d;
 import com.team254.lib.util.CheesyDriveHelper;
 import com.team254.lib.util.CrashTracker;
 import com.team254.lib.util.LatchedBoolean;
+import com.team254.lib.util.Util;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Timer;
@@ -259,7 +260,14 @@ public class Robot extends IterativeRobot {
                     mForklift.deploy();
                 }
 
-                mSuperstructure.setHangThrottle(mControlBoard.getElevatorThrottle());
+                final double kElevatorThrottleDeadband = 0.3;
+                double elevatorThrottle = mControlBoard.getElevatorThrottle();
+                if(Math.abs(elevatorThrottle) < kElevatorThrottleDeadband) {
+                    elevatorThrottle = 0;
+                } else {
+                    elevatorThrottle = (elevatorThrottle - Math.signum(elevatorThrottle) * kElevatorThrottleDeadband) / (1.0 - kElevatorThrottleDeadband);
+                }
+                mSuperstructure.setHangThrottle(elevatorThrottle);
 
                 if (mLowShiftPressed.update(mControlBoard.getElevatorLowShift())) {
                    mSuperstructure.setElevatorLowGear();
@@ -269,6 +277,7 @@ public class Robot extends IterativeRobot {
                 mSuperstructure.setUnlockHookSolenoid(true);
             } else {
                 mSuperstructure.setUnlockHookSolenoid(false);
+                mForklift.retract();
                 mLED.setWantedAction(LED.WantedAction.DISPLAY_INTAKE);
 
                 // Intake/Shoot
