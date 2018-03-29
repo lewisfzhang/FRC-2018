4f36994330974cea01232469f125894017fd437a
Solomon Chang <changsolomon8@gmail.com>
2018-03-28 21:12:32 -0700

    Tune color on led

--- src/main/java/com/team254/frc2018/states/LEDState.java
+++ src/main/java/com/team254/frc2018/states/LEDState.java
@@ -11,7 +11,7 @@ public class LEDState {
 
     public static final LEDState kHanging = new LEDState(0.0, 0.3, 1.0);
 
-    public static final LEDState kWantsCube = new LEDState(0.0, 1.0, 1.0);
+    public static final LEDState kWantsCube = new LEDState(0.0, 0.25, 1.0);
 
     public LEDState() {}
 
