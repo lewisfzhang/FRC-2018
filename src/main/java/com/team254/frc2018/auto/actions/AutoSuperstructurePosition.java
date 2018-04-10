bae8f80def11d63655006da3d46af2c9e14f85ac
SuperMario210 <Sup3rMar1o210@gmail.com>
2018-04-09 23:47:23 -0700

    fixed auto superstructure position action

--- src/main/java/com/team254/frc2018/auto/actions/AutoSuperstructurePosition.java
+++ src/main/java/com/team254/frc2018/auto/actions/AutoSuperstructurePosition.java
@@ -1,64 +1,49 @@
 package com.team254.frc2018.auto.actions;
 
-import com.team254.frc2018.AutoFieldState;
+import com.team254.frc2018.statemachines.SuperstructureStateMachine;
+import com.team254.frc2018.states.SuperstructureConstants;
 import com.team254.frc2018.states.SuperstructureState;
 import com.team254.frc2018.subsystems.CheesyVision2;
 import com.team254.frc2018.subsystems.Superstructure;
 import com.team254.lib.util.Util;
+import edu.wpi.first.wpilibj.Timer;
 
 public class AutoSuperstructurePosition implements Action {
-    private static final Superstructure mSuperstructure = Superstructure.getInstance();
     private static final CheesyVision2 mCheesyVision2 = CheesyVision2.getInstance();
+    private static final Superstructure mSuperstructure = Superstructure.getInstance();
     private static final double kHeightEpsilon = 3.0;
     private static final double kAngleEpsilon = 10.0;
 
-    private final double mLowHeight, mNeutralHeight, mHighHeight, mDefaultHeight;
+    private final int mNumCubes;
+    private double mHeight;
     private final double mAngle;
     private final boolean mWaitForCompletion;
-    
-    private double mDesiredHeight;
 
-    public AutoSuperstructurePosition(double lowHeight, double neutralHeight, double highHeight,
-                                    double defaultHeight, double angle, boolean waitForCompletion) {
-        mLowHeight = lowHeight;
-        mNeutralHeight = neutralHeight;
-        mHighHeight = highHeight;
-        mDefaultHeight = defaultHeight;
+    public AutoSuperstructurePosition(int numCubes, double angle, boolean waitForCompletion) {
+        mNumCubes = numCubes;
         mAngle = angle;
         mWaitForCompletion = waitForCompletion;
-    }
-    
-    protected double getDesiredHeight() {
-        if (mCheesyVision2.getError()) return mDefaultHeight;
-        
-        double tip = mCheesyVision2.getTip();
-        AutoFieldState state = AutoFieldState.getInstance();
-        if (state.getScaleSide() == AutoFieldState.Side.LEFT)
-            tip = -tip;
-        
-        double height = mNeutralHeight;
-        if (tip < 0) height = mLowHeight;
-        if (tip > 0) height = mHighHeight;
-        return height;
     }
 
     @Override
     public void start() {
-        mDesiredHeight = getDesiredHeight();
-        mSuperstructure.setDesiredHeight(mDesiredHeight);
+        mHeight = mCheesyVision2.getDesiredHeight((mAngle == SuperstructureConstants.kScoreBackwardsAngle), mNumCubes);
+        mSuperstructure.setDesiredHeight(mHeight);
         mSuperstructure.setDesiredAngle(mAngle);
     }
 
     @Override
     public void update() {
+        mHeight = mCheesyVision2.getDesiredHeight((mAngle == SuperstructureConstants.kScoreBackwardsAngle), mNumCubes);
+        mSuperstructure.setDesiredHeight(mHeight);
     }
 
     @Override
     public boolean isFinished() {
         if(mWaitForCompletion) {
             SuperstructureState state = mSuperstructure.getObservedState();
-            return Util.epsilonEquals(state.height, mDesiredHeight, kHeightEpsilon) &&
-                   Util.epsilonEquals(state.angle, mAngle, kAngleEpsilon);
+            return Util.epsilonEquals(state.height, mHeight, kHeightEpsilon) &&
+                    Util.epsilonEquals(state.angle, mAngle, kAngleEpsilon);
         } else {
             return true;
         }
